3a8558abb1755742ae62f58f78e7d82829d2e164
Filip Johnsson <Filip.johnsson1989@gmail.com>
2022-04-19 11:33:39 +0200

    Add Data Validation to the Form

--- BugTrackerUI/Bug.cs
+++ BugTrackerUI/Bug.cs
@@ -9,11 +9,11 @@ namespace BugTrackerUI
     public class Bug
     {
         public int Id { get; set; }
-
+        [Required]
         public string Title { get; set; }
-
+        [MinLength(10)]
         public string Description { get; set; }
-
+        [Range(1,5)]
         public int Priority { get; set; }
     }
 }
